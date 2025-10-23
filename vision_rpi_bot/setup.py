962743ab81fb186d3c78371da4ab88b297706429
pauzan1234 <pauzan.muh@gmail.com>
2025-10-23 08:18:59 +0700

    section 1 complete

---
+++ vision_rpi_bot/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+package_name = 'vision_rpi_bot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pauzan',
+    maintainer_email='pauzan@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+                'publisher_node = vision_rpi_bot.publisher:main',
+                'subscriber_node = vision_rpi_bot.subscriber:main',
+        ],
+    },   
+)
+
